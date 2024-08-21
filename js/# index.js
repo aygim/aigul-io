5e79bff685026442d45d2df2315c6ba61f120abc
Angel <stetchi.aigul@gmail.com>
2024-08-20 22:08:32 -0400

    some changes

--- js/# index.js
+++ js/# index.js
@@ -1,8 +1,8 @@
 const today = new Date();
-const thisYeasr = today.getFullYear();
+const thisYear = today.getFullYear();
 const footer = document.querySelector('footer');
 const copyright = document.createElement('p');
-copyright.innerHTML = ' Aigul Stetchi &copy; ${thisYear}';
+copyright.innerHTML = `Aigul Stetchi &copy; ${thisYear}`;
 footer.appendChild(copyright);
 
 
@@ -10,8 +10,9 @@ footer.appendChild(copyright);
 const skills = ["JavaScript", "HTML", "CSS", "Adobe Photoshop", "GitHub"];
 const skillsSection = document.getElementById("skills");
 const skillsList = skillsSection.querySelector('ul');
-for (let i = 0; i <skills.length; i++){
+
+for (let i = 0; i < skills.length; i++) {
     let skill = document.createElement('li');
     skill.innerHTML = skills[i];
-    skillsList.appendChild(skill)
+    skillsList.appendChild(skill);
 }
