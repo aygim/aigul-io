bbd792a1d2cf5dd42ba43622cfcf38633fd9a82c
Angel <stetchi.aigul@gmail.com>
2024-08-27 14:02:43 -0400

    working on js

--- js/index.js
+++ js/index.js
@@ -16,3 +16,53 @@ for (let i = 0; i < skills.length; i++) {
     skill.innerHTML = skills[i];
     skillsList.appendChild(skill);
 }
+
+//Handle Message Form Submit
+const messageForm = document.querySelector('form[name = "leave_message"]');
+messageForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const userName = e.target.usersName.value;
+    const email = e.target.usersEmail.value;
+    const messages = e.target.usersMessage.value;
+    console.log("Name:", userName);
+    console.log("Email:", email);
+    console.log("Message:", messages);
+
+
+//Display Messages in List
+const messageSection = document.getElementById("messages");
+const messageList = messageSection.querySelector('ul');
+const newMessage = document.createElement ('li');
+newMessage.innerHTML = `<a href = "mailto:${email}"> ${userName} </a>
+<span>wrote: ${messages}</span>`;
+
+//Remove button
+const removeButton = document.createElement('button');
+    removeButton.innerText = 'remove';
+    removeButton.type = 'button';
+    removeButton.addEventListener('click', (e) => {
+        entry.remove();
+//Hide the #messages section,
+
+        if (messageList.childElementCount === 0) {
+            messageSection.style.display = "none";
+        
+        }
+    });
+
+
+//Create an "edit" button
+const editButton = document.createElement('button');
+editButton.innerText = 'edit';
+editButton.type = 'button';
+editButton.addEventListener('click', (e) => {
+    const updateMessages = prompt('Please edit your message');
+    let editedMessage = newMessage.querySelector ('span');
+    editedMessage.textContent = 'wrote: ${updateMessage}';
+});
+  newMessage.appendChild(removeButton);
+  newMessage.appendChild(editButton);
+  newMessage.appendChild(newMessage);
+  messageSection.style.display = "block";
+  messageForm.requestFullscreen();
+})
